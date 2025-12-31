bcf6c0cdb8536e815e728114d8b6d703bb568e0f
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-12-31 13:11:07 +0100

    Updated package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    /* config options here */
     images: {
         remotePatterns: [
             {
@@ -9,7 +8,12 @@ const nextConfig: NextConfig = {
                 hostname: "images.unsplash.com"
             }
         ]
-    }
+    },
+    experimental: {
+        workerThreads: false,
+        cpus: 1
+    },
+    productionBrowserSourceMaps: false
 }
 
 export default nextConfig
