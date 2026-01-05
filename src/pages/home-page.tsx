6a5ce6f774944e5c467f7c77b59a564f28eb6087
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-01-05 12:15:26 +0100

    Updated home-page.tsx

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -9,63 +9,21 @@ import { ServicesSection } from '@/components/layout/sections/services'
 import { TeamSection } from '@/components/layout/sections/team'
 import { TestimonialSection } from '@/components/layout/sections/testimonial'
 import LogoCloud from '@/components/logo-cloud'
-import { WaveDivider } from '@/components/ui/wave-divider'
 
 export default function HomePage() {
   return (
     <>
-      <div className="relative">
-        <HeroSection />
-        <WaveDivider variant={1} position="bottom" />
-      </div>
-
-      <div className="relative">
-        <LogoCloud />
-        <WaveDivider variant={2} position="top" flip />
-      </div>
-
-      <div className="relative">
-        <BenefitsSection />
-        <WaveDivider variant={1} position="bottom" />
-      </div>
-
-      <div className="relative">
-        <FeaturesSection />
-        <WaveDivider variant={2} position="bottom" flip />
-      </div>
-
-      <div className="relative">
-        <ServicesSection />
-        <WaveDivider variant={1} position="bottom" />
-      </div>
-
-      <div className="relative">
-        <TestimonialSection />
-        <WaveDivider variant={2} position="bottom" flip />
-      </div>
-
-      <div className="relative">
-        <TeamSection />
-        <WaveDivider variant={1} position="bottom" />
-      </div>
-
-      <div className="relative">
-        <CommunitySection />
-        <WaveDivider variant={2} position="bottom" flip />
-      </div>
-
-      <div className="relative">
-        <PricingSection />
-        <WaveDivider variant={1} position="bottom" />
-      </div>
-
-      <div className="relative">
-        <ContactSection />
-      </div>
-
-      <div className="relative">
-        <FAQSection />
-      </div>
+      <HeroSection />
+      <LogoCloud />
+      <BenefitsSection />
+      <FeaturesSection />
+      <ServicesSection />
+      <TestimonialSection />
+      <TeamSection />
+      <CommunitySection />
+      <PricingSection />
+      <ContactSection />
+      <FAQSection />
     </>
   )
 }
