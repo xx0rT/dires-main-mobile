e3532210c80025176e831a58bc54d88816d16ac8
Troxx <121587043+xx0rT@users.noreply.github.com>
2026-02-11 12:26:10 +0100

    Updated footer.tsx

--- src/components/layout/sections/footer.tsx
+++ src/components/layout/sections/footer.tsx
@@ -253,7 +253,7 @@ const Footer31 = ({ className }: Footer31Props) => {
               className="text-[150px] font-bold tracking-tighter text-neutral-100"
               style={{ fontFamily: "inherit", opacity: 0.15 }}
             >
-              DIRES.CZ
+              <a href="#"">DIRES.CZ</a>
             </text>
           </svg>
         </motion.div>
